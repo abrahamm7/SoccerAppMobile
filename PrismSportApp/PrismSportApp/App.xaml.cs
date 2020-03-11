bc483160cd92dd33624f8eb8e7c70452bd07d6b6
Abraham Morillo <abrahammorillo777@gmail.com>
2020-03-10 20:10:34 -0400

    Add project files.

---
+++ PrismSportApp/PrismSportApp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrismSportApp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            //NavigationService.NavigateAsync(new Uri("HomePage", UriKind.Absolute));
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
+        }
+    }
+}
