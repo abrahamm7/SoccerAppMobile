64dd6f8290a225a9ccdddab93230ef60c892942b
Abraham Morillo <abrahammorillo777@gmail.com>
2020-04-04 22:45:52 -0400

    Charts

--- PrismSportApp/PrismSportApp/ViewModels/TeamInfoViewModel.cs
+++ PrismSportApp/PrismSportApp/ViewModels/TeamInfoViewModel.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Xml.Linq;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Microcharts.Forms;
+using Microcharts;
 
 namespace PrismSportApp.ViewModels
 {
@@ -18,9 +20,69 @@ namespace PrismSportApp.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public Table Table { get; set; } = new Table();
-        
+        public Chart RadielGaugeChartSample => new RadialGaugeChart()
+        {
+            Entries = new[]
+            {
+                new Microcharts.Entry(Table.Won)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#4CAF50"),
+                    Label = "Won",
+                    ValueLabel = Table.Won.ToString(),
+                },
+                new Microcharts.Entry(Table.Draw)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#9E9E9E"),
+                    Label = "Draw",
+                    ValueLabel = Table.Draw.ToString(),
+                },
+                new Microcharts.Entry(Table.Lost)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#F44336"),                    
+                    Label = "Lost",
+                    ValueLabel = Table.Lost.ToString(),
+                },
+                new Microcharts.Entry(Table.PlayedGames)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#FFC107"),
+                    Label = "Played Games",
+                    ValueLabel = Table.PlayedGames.ToString(),
+                },
+            },
+            LineSize = 30,            
+            LabelTextSize = 45,
+            MaxValue = Table.PlayedGames
+        };
+
+        public Chart RadielGaugeGoals => new RadialGaugeChart()
+        {
+            Entries = new[]
+            {
+                new Microcharts.Entry(Table.GoalsFor)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#303F9F"),
+                    Label = "Goals",
+                    ValueLabel = Table.GoalsFor.ToString(),
+                },
+                new Microcharts.Entry(Table.GoalsAgainst)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#4CAF50"),
+                    Label = "Against",
+                    ValueLabel = Table.GoalsAgainst.ToString(),
+                },
+                new Microcharts.Entry(Table.GoalDifference)
+                {
+                    Color = SkiaSharp.SKColor.Parse("#9C27B0"),
+                    Label = "Goals Difference",
+                    ValueLabel = Table.GoalDifference.ToString(),
+                },               
+            },
+            LineSize = 30,
+            LabelTextSize = 45,
+            MaxValue = Table.GoalsFor
+        };
+
         public ICommand Save { get; set; }
-       
         ISqliteInterface Sqlite;
         public void OnNavigatedFrom(INavigationParameters parameters)
         {
@@ -33,16 +95,8 @@ namespace PrismSportApp.ViewModels
         }
         public TeamInfoViewModel(ISqliteInterface sqliteInterface)
         {
-            Save = new Command(AddTeam);
-            Sqlite = sqliteInterface;          
+            
         }
-
-        async void AddTeam()
-        {
-            var x = Sqlite.GetConnection();
-            //Table.Team.Name = TeamName;
-            //Table.Team.CrestUrl= Logo;
-            //conn.Insert(Table);
-        }
+        
     }
 }
