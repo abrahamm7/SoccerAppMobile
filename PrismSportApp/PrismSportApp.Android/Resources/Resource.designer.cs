e01287f7099681128b3d94c3768bd6a4cd20c67f
Abraham Morillo <abrahammorillo777@gmail.com>
2020-08-31 17:04:49 -0400

    .

--- PrismSportApp/PrismSportApp.Android/Resources/Resource.designer.cs
+++ PrismSportApp/PrismSportApp.Android/Resources/Resource.designer.cs
@@ -26743,16 +26743,19 @@ namespace PrismSportApp.Droid
 			public const int icons8_male_user = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int icons8_stadium = 2131165345;
+			public const int icons8_plus = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int icons8_stadium_16 = 2131165346;
+			public const int icons8_stadium = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int icons8_stadium_32 = 2131165347;
+			public const int icons8_stadium_16 = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int icons8_star = 2131165348;
+			public const int icons8_stadium_32 = 2131165348;
+			
+			// aapt resource value: 0x7F0700A5
+			public const int icons8_star = 2131165349;
 			
 			// aapt resource value: 0x7F07007E
 			public const int ic_mtrl_chip_checked_black = 2131165310;
@@ -26853,134 +26856,134 @@ namespace PrismSportApp.Droid
 			// aapt resource value: 0x7F07009E
 			public const int ic_plugin_sc_update = 2131165342;
 			
-			// aapt resource value: 0x7F0700A5
-			public const int LaLiga = 2131165349;
-			
 			// aapt resource value: 0x7F0700A6
-			public const int ligue1 = 2131165350;
+			public const int LaLiga = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int Liverpool_FC = 2131165351;
+			public const int ligue1 = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int MaterialActivityIndicatorBackground = 2131165352;
+			public const int Liverpool_FC = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int MaterialProgressBar = 2131165353;
+			public const int MaterialActivityIndicatorBackground = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int mtrl_snackbar_background = 2131165354;
+			public const int MaterialProgressBar = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int mtrl_tabs_default_indicator = 2131165355;
+			public const int mtrl_snackbar_background = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int navigation_empty_icon = 2131165356;
+			public const int mtrl_tabs_default_indicator = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int Next = 2131165357;
+			public const int navigation_empty_icon = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int notification_action_background = 2131165358;
+			public const int Next = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int notification_bg = 2131165359;
+			public const int notification_action_background = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int notification_bg_low = 2131165360;
+			public const int notification_bg = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int notification_bg_low_normal = 2131165361;
+			public const int notification_bg_low = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int notification_bg_low_pressed = 2131165362;
+			public const int notification_bg_low_normal = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int notification_bg_normal = 2131165363;
+			public const int notification_bg_low_pressed = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int notification_bg_normal_pressed = 2131165364;
+			public const int notification_bg_normal = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int notification_icon_background = 2131165365;
+			public const int notification_bg_normal_pressed = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int notification_template_icon_bg = 2131165366;
+			public const int notification_icon_background = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int notification_template_icon_low_bg = 2131165367;
+			public const int notification_template_icon_bg = 2131165367;
 			
 			// aapt resource value: 0x7F0700B8
-			public const int notification_tile_bg = 2131165368;
+			public const int notification_template_icon_low_bg = 2131165368;
 			
 			// aapt resource value: 0x7F0700B9
-			public const int notify_panel_notification_icon_bg = 2131165369;
+			public const int notification_tile_bg = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int players = 2131165370;
+			public const int notify_panel_notification_icon_bg = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int plusBlack = 2131165371;
+			public const int players = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int portugal = 2131165372;
+			public const int plusBlack = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int PremierLeague = 2131165373;
+			public const int portugal = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int Prev = 2131165374;
+			public const int PremierLeague = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int remove = 2131165375;
+			public const int Prev = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int SeriaA = 2131165376;
+			public const int remove = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int setti = 2131165377;
+			public const int SeriaA = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int soccer = 2131165378;
-			
-			// aapt resource value: 0x7F0700C4
-			public const int Splashfutbol = 2131165380;
+			public const int setti = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int splash_background = 2131165379;
+			public const int soccer = 2131165379;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int star = 2131165381;
+			public const int Splashfutbol = 2131165381;
+			
+			// aapt resource value: 0x7F0700C4
+			public const int splash_background = 2131165380;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int taza = 2131165382;
+			public const int star = 2131165382;
 			
 			// aapt resource value: 0x7F0700C7
-			public const int tooltip_frame_dark = 2131165383;
+			public const int taza = 2131165383;
 			
 			// aapt resource value: 0x7F0700C8
-			public const int tooltip_frame_light = 2131165384;
+			public const int tooltip_frame_dark = 2131165384;
 			
 			// aapt resource value: 0x7F0700C9
-			public const int Trash = 2131165385;
+			public const int tooltip_frame_light = 2131165385;
 			
 			// aapt resource value: 0x7F0700CA
-			public const int trofeo = 2131165386;
+			public const int Trash = 2131165386;
 			
 			// aapt resource value: 0x7F0700CB
-			public const int uefachampions = 2131165387;
+			public const int trofeo = 2131165387;
 			
 			// aapt resource value: 0x7F0700CC
-			public const int Up = 2131165388;
+			public const int uefachampions = 2131165388;
 			
 			// aapt resource value: 0x7F0700CD
-			public const int user = 2131165389;
+			public const int Up = 2131165389;
 			
 			// aapt resource value: 0x7F0700CE
-			public const int user16px = 2131165390;
+			public const int user = 2131165390;
 			
 			// aapt resource value: 0x7F0700CF
-			public const int worldcup = 2131165391;
+			public const int user16px = 2131165391;
+			
+			// aapt resource value: 0x7F0700D0
+			public const int worldcup = 2131165392;
 			
 			static Drawable()
 			{
